7807c106580db4a466b4a1e29d53cb953e8cf2c6
Tim Voronov <ziflex@gmail.com>
2020-08-07 21:41:33 -0400

    Updated stdlib doc markup

--- tools/docs/stdlib/extract.py
+++ tools/docs/stdlib/extract.py
@@ -6,126 +6,132 @@ from comment_parser import comment_parser
 
 class Extract:
 
-  def __init__(self,
-               content,
-               token_name,
-               token_package,
-               file_name,
-               line_number):
+    def __init__(self,
+                 content,
+                 token_name,
+                 token_package,
+                 file_name,
+                 line_number):
+        self.content = content
+        self.token_name = token_name
+        self.token_package = token_package
+        self.file_name = file_name
+        self.line_number = line_number
 
-    self.content = content
-    self.token_name = token_name
-    self.token_package = token_package
-    self.file_name = file_name
-    self.line_number = line_number
+    def __str__(self):
+        r_content = repr(self.content)
+        r_token = repr(self.token_name)
+        r_file_name = repr(self.file_name)
+        r_line_number = repr(self.line_number)
 
-  def __str__(self):
-    r_content = repr(self.content)
-    r_token = repr(self.token_name)
-    r_file_name = repr(self.file_name)
-    r_line_number = repr(self.line_number)
+        return f'Extract({r_content}, {r_token}, {r_file_name}, {r_line_number})'
 
-    return f'Extract({r_content}, {r_token}, {r_file_name}, {r_line_number})'
-
-  def __repr__(self):
-    return str(self)
+    def __repr__(self):
+        return str(self)
 
 
 stdlib_method_signature = re.compile(
-  'func (.+)\([\w]+ context\.Context, [\w]+ \.\.\.core\.Value\) \(core\.Value, error\)'
+    'func (.+)\([\w]+ context\.Context, [\w]+ \.\.\.core\.Value\) \(core\.Value, error\)'
+)
+
+stdlib_testing_signature = re.compile(
+    'var\s+(\w+)\s+=\s+base.Assertion'
 )
 
 
 def get_doc_extracts(go_file, directory, token_impl_map):
-  go_lines, go_code = {}, None
+    go_lines, go_code = {}, None
 
-  # Read the contents of a file as a string and also index lines by line
-  # numbers. This is required later on to process string by lines.
-  with open(go_file, 'r') as file:
-    for no, line in enumerate(file.readlines()):
-      go_lines[no] = line
+    # Read the contents of a file as a string and also index lines by line
+    # numbers. This is required later on to process string by lines.
+    with open(go_file, 'r') as file:
+        for no, line in enumerate(file.readlines()):
+            go_lines[no] = line
 
-    file.seek(0)
-    go_code = file.read()
+        file.seek(0)
+        go_code = file.read()
 
-  # Extract all comments in a go source file. Each line in the comment is
-  # returned as Comment instance, the comments are thus, not grouped.
-  comments = comment_parser.extract_comments_from_str(
-    go_code,
-    mime='text/x-go',
-  )
-
-  comment_groups = []
-  current_line = None
-
-  # Group the comment lines as block of comments. Lines that have consecutive
-  # line numbers are assumed to belong to the same comment block.
-  for comment in comments:
-    new_line = comment.line_number()
-
-    if current_line is None or new_line != current_line + 1:
-      comment_groups += [[]]
-
-    comment_groups[-1].append(comment)
-    current_line = new_line
-
-  doc_groups = []
-
-  # Get the entity that a comment block is talking about and check if
-  # it belongs to an entity that we are interested in and aggregate
-  # them.
-  for group in comment_groups:
-    group_end = group[-1].line_number()
-    comment_related_to = go_lines.get(group_end)
-
-    if not comment_related_to:
-      continue
-
-    match = stdlib_method_signature.search(
-      comment_related_to,
+    # Extract all comments in a go source file. Each line in the comment is
+    # returned as Comment instance, the comments are thus, not grouped.
+    comments = comment_parser.extract_comments_from_str(
+        go_code,
+        mime='text/x-go',
     )
 
-    if match is None:
-      continue
+    comment_groups = []
+    current_line = None
 
-    doc_groups.append((group, match))
+    # Group the comment lines as block of comments. Lines that have consecutive
+    # line numbers are assumed to belong to the same comment block.
+    for comment in comments:
+        new_line = comment.line_number()
 
-  # Collate all the gathered information about the extract and build
-  # Extracts.
-  for (group, match) in doc_groups:
-    content = list(map(
-      lambda comment: comment.text().strip(),
-      group,
-    ))
+        if current_line is None or new_line != current_line + 1:
+            comment_groups += [[]]
 
-    file_name = os.path.relpath(go_file, directory)
-    token_package = os.path.dirname(file_name)
+        comment_groups[-1].append(comment)
+        current_line = new_line
 
-    impl_name = match.group(1)
-    token_name = token_impl_map.get(impl_name, None)
+    doc_groups = []
 
-    if token_name is None:
-      token_name = impl_name
-      print(f'Token against {impl_name} was not found')
+    # Get the entity that a comment block is talking about and check if
+    # it belongs to an entity that we are interested in and aggregate
+    # them.
+    for group in comment_groups:
+        group_end = group[-1].line_number()
+        comment_related_to = go_lines.get(group_end)
 
-    # TODO: token_name should be the name the method registers itself with
-    # instead of the name of the method that implements it.
-    yield Extract(
-      content=content,
-      token_name=token_name,
-      token_package=token_package,
-      line_number=group[-1].line_number(),
-      file_name=file_name,
-    )
+        if not comment_related_to:
+            continue
+
+        match = stdlib_method_signature.search(
+            comment_related_to,
+        )
+
+        if match is None:
+            match = stdlib_testing_signature.search(comment_related_to)
+
+            if match is None:
+                continue
+
+        doc_groups.append((group, match))
+
+    # Collate all the gathered information about the extract and build
+    # Extracts.
+    for (group, match) in doc_groups:
+        content = list(map(
+            lambda comment: comment.text().strip(),
+            group,
+        ))
+
+        file_name = os.path.relpath(go_file, directory)
+        token_package = os.path.dirname(file_name)
+
+        impl_name = match.group(1)
+        token_name = token_impl_map.get(impl_name, None)
+
+        if token_name is None:
+            token_name = impl_name
+            print(f'Token against {impl_name} was not found')
+
+        # TODO: token_name should be the name the method registers itself with
+        # instead of the name of the method that implements it.
+        yield Extract(
+            content=content,
+            token_name=token_name,
+            token_package=token_package,
+            line_number=group[-1].line_number(),
+            file_name=file_name,
+        )
 
 
 def get_token_impl_map(go_file):
-  # Extract all implementations and token names from indices.
+    # Extract all implementations and token names from indices.
 
-  regex = re.compile(
-    '"(?P<token>[A-Z0-9_]+)":[ \t]*(?P<impl>[\w\d]+)'
-  )
+    regex = re.compile(
+        '"(?P<token>[A-Z0-9_]+)":[ \t]*(?P<impl>[\w\d]+)'
+    )
 
-  with open(go_file, 'r') as file:
-    for match in regex.finditer(file.read()):
-      yield { match.group('impl'): match.group('token') }
+    with open(go_file, 'r') as file:
+        for match in regex.finditer(file.read()):
+            yield {match.group('impl'): match.group('token')}
--- tools/docs/stdlib/transform.py
+++ tools/docs/stdlib/transform.py
@@ -1,119 +1,175 @@
 import re
 
 
-def de_parenthesis(text):
-  if text.startswith('('):
-    text = text[1:]
+def normalize(text):
+    if text is not None:
+        return text.strip()
 
-  if text.endswith(')'):
-    text = text[:-1]
+    return None
 
-  return text
+
+def normalize_type(text):
+    text = normalize(text)
+
+    if text is None:
+        text = ""
+
+    if text.startswith('{'):
+        text = text[1:]
+
+    if text.endswith('}'):
+        text = text[:-1]
+
+    return text
+
+
+def process_name(text):
+    if text is None:
+        return None, False, None
+
+    is_optional = text.startswith('[')
+    default_value = None
+
+    if not is_optional:
+        return text, False, None
+
+    regex = re.compile(
+        '\[(?P<name>[A-Za-z]+)(?P<eq>=)?(?P<value>.+)?\]',
+    )
+
+    match = regex.search(text)
+    name = normalize(match.group('name'))
+
+    try:
+        default_value = normalize(match.group('value'))
+    except:
+        pass
+
+    return name, is_optional, normalize(default_value)
 
 
 def preprocess_line(target):
-  target = target.replace('@params', '@param')
-  target = target.replace('@returns', '@return')
+    target = target.replace('@params', '@param')
+    target = target.replace('@returns', '@return')
 
-  return target
+    return target
 
 
-def process_decorated(regex, target):
-  match = regex.search(target)
+def parse(regex, target):
+    match = regex.search(target)
 
-  arg_name = None
-  arg_type = match.group('type')
-  arg_desc = match.group('desc')
+    if match is None:
+        print("Failed to match string: " + target)
+        return None, None, None
 
-  try:
-    arg_name = match.group('name')
-  except:
-    pass
+    arg_name = None
+    arg_type = normalize(match.group('type'))
+    arg_desc = normalize(match.group('desc'))
 
-  arg_type = de_parenthesis(
-    arg_type,
-  )
+    try:
+        arg_name = normalize(match.group('name'))
+    except:
+        pass
 
-  arg_types = list(map(
-    str.strip,
-    filter(None, arg_type.split('|')),
-  ))
+    arg_type = normalize_type(
+        arg_type,
+    )
 
-  arg_desc = arg_desc.capitalize()
+    arg_types = list(map(
+        str.strip,
+        filter(None, arg_type.split('|')),
+    ))
 
-  return arg_name, arg_types, arg_desc
+    arg_desc = arg_desc.capitalize()
+
+    return arg_name, arg_types, arg_desc
 
 
 def process_returns(returns):
-  if returns is None:
-    return None
+    if returns is None:
+        return None
 
-  regex = re.compile(
-    '@return (?P<type>\(.+\)) - (?P<desc>.+)',
-  )
-
-  arg_name, arg_types, arg_desc = process_decorated(
-    regex,
-    returns,
-  )
-
-  return {
-    'name': arg_name,
-    'type': arg_types,
-    'desc': arg_desc,
-  }
-
-
-def process_params(params):
-  regex = re.compile(
-    '@param (?P<name>.+) (?P<type>\(.+\)) - (?P<desc>.+)',
-  )
-
-  for param in params:
-    arg_name, arg_types, arg_desc = process_decorated(
-      regex,
-      param,
+    regex = re.compile(
+        '@return\s*(?P<type>{.+})\s*-\s*(?P<desc>.+)',
     )
 
-    yield {
-      'name': arg_name,
-      'type': arg_types,
-      'desc': arg_desc,
+    arg_name, arg_types, arg_desc = parse(
+        regex,
+        returns,
+    )
+
+    if arg_desc is None:
+        return None
+
+    return {
+        'name': arg_name,
+        'type': arg_types,
+        'desc': arg_desc,
     }
 
 
+def process_params(params):
+    regex = re.compile(
+        '@param\s*(?P<type>{.+})\s*(?P<name>.+)?\s*-\s*(?P<desc>.+)',
+    )
+
+    for num, param in enumerate(params, start=1):
+        arg_name, arg_types, arg_desc = parse(
+            regex,
+            param,
+        )
+
+        arg_optional = False
+        arg_default_value = None
+
+        if arg_name is not None:
+            arg_name, arg_optional, arg_default_value = process_name(arg_name)
+        else:
+            print(param)
+            arg_name = "param" + str(num)
+
+        if arg_types is not None:
+            yield {
+                'name': arg_name,
+                'type': arg_types,
+                'desc': arg_desc,
+                'optional': arg_optional,
+                'default_value': arg_default_value
+            }
+
+
 def transform_extract(extract):
-  content, params, returns = [], [], None
+    content, params, returns = [], [], None
 
-  for line in extract.content:
-    line = preprocess_line(line.strip())
+    for line in extract.content:
+        line = preprocess_line(line.strip())
 
-    if line.startswith('@param'):
-      params.append(line)
-      continue
+        if line.startswith('@param'):
+            params.append(line)
+            continue
 
-    if line.startswith('@return'):
-      returns = line
-      continue
+        if line.startswith('@return'):
+            returns = line
+            continue
 
-    if returns is not None:
-      returns = f'{returns} {line}'
-      continue
+        if returns is not None:
+            returns = f'{returns} {line}'
+            continue
 
-    if len(params) > 0:
-      params[-1] = f'{params[-1]} {line}'
-      continue
+        if len(params) > 0:
+            params[-1] = f'{params[-1]} {line}'
+            continue
 
-    if len(params) == 0:
-      content.append(line)
-      continue
+        if len(params) == 0:
+            content.append(line)
+            continue
 
-  content = ' '.join(content)
-  returns = process_returns(returns)
-  params = list(process_params(params))
+    content = ' '.join(content)
+    returns = process_returns(returns)
+    params = list(process_params(params))
 
-  return {
-    'desc': content,
-    'params': params,
-    'returns': returns,
-  }
+    return {
+        'desc': content,
+        'params': params,
+        'returns': returns,
+    }
